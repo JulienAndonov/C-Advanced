e2b7077334bbe5354b0ea08c1f5fe922345a0319
Julien Valeriev Andonov <y9ann5@cz.ibm.com>
2019-01-25 11:35:56 +0100

    25012019

--- Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
+++ Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
@@ -1,44 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 namespace P07_VLogger
 {
+
+
+    class VloggerData
+    {
+        public int followers { get; set; }
+        public int following { get; set; }
+        public SortedSet<string> followersNames { get; set; }
+
+
+        public VloggerData(int followers, int following, SortedSet<string> followersNames)
+        {
+            this.followers = followers;
+            this.following = following;
+            this.followersNames = followersNames;
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> vloggerToFollowers = new Dictionary<string, List<string>>();
-            Dictionary<string, List<string>> vloggerToFollowing = new Dictionary<string, List<string>>();
+            Dictionary<string, VloggerData> vloggers = new Dictionary<string, VloggerData>();
+
 
 
 
             while (true)
             {
-                var command = Console.ReadLine();
-                if (command == "statistics")
+                var entryCommand = Console.ReadLine();
+
+
+                if (entryCommand == "Statistics")
                 {
+
+
+
                     break;
                 }
 
 
-                var entries = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                var vlogerName = entries[0];
+                var entries = entryCommand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var vloggerName = entries[0];
                 var action = entries[1];
-                var 
+                var followed = entries[2];
 
 
 
+                switch (action)
+                {
 
+                    case "joined":
+                        if (!vloggers.ContainsKey(vloggerName))
+                        {
+                            vloggers[vloggerName] = new VloggerData(0, 0, new SortedSet<string>());
+                        }
+                        break;
+                    case "followed":
+                        if (vloggers.ContainsKey(vloggerName) && vloggers.ContainsKey(followed) && vloggerName != followed)
+                        {
+                            vloggers[vloggerName].following++;
+                            vloggers[followed].followers++;
+                            vloggers[followed].followersNames.Add(vloggerName);
+                        }
+                        break;
+                }
             }
-
-
-
-
-
-
-
-
         }
     }
 }
