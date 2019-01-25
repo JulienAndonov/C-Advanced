f0196f2ed0e5ce2127efcf0a8648f2748fb12e1c
Julien Valeriev Andonov <y9ann5@cz.ibm.com>
2019-01-25 11:56:59 +0100

    25012019v5

---
+++ Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_VLogger
+{
+
+    class VloggerData
+    {
+        public int followed { get; set; }
+        public int following { get; set; }
+        public SortedSet<string> followersNames { get; set; }
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, VloggerData> vloggers = new Dictionary<string, VloggerData>();
+
+
+
+            while (true)
+            {
+                var entryCommand = Console.ReadLine();
+
+
+                if (entryCommand == "Statistics")
+                {
+                    break;
+                }
+
+
+                var entries = entryCommand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var vloggerName = entries[0];
+                var action = entries[1];
+                var scndName = entries[2];
+
+                
+
+
+            }
+
+
+
+
+        }
+    }
+}
