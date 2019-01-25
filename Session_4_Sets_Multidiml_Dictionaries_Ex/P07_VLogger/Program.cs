2a6411b20764e77f88ad96e04eb4cc14947512fd
JulienAndonov <julien.v.andonov@googlemail.com>
2019-01-25 11:49:50 +0100

    25012019v3

--- Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
+++ Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
@@ -8,16 +8,18 @@ namespace P07_VLogger
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> vloggerToFollowers = new Dictionary<string, List<string>>();
-            Dictionary<string, List<string>> vloggerToFollowing = new Dictionary<string, List<string>>();
+            Dictionary<string, HashSet<string>> vloggerFollowers = new Dictionary<string, HashSet<string>>();
+            Dictionary<string, HashSet<string>> vloggerFolloed = new Dictionary<string, HashSet<string>>();
 
 
 
             while (true)
             {
                 var command = Console.ReadLine();
-                if (command == "statistics")
+                if (command == "Statistics")
                 {
+
+
                     break;
                 }
 
@@ -25,7 +27,28 @@ namespace P07_VLogger
                 var entries = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 var vlogerName = entries[0];
                 var action = entries[1];
-                var 
+                var followed = entries[2];
+
+                switch (action)
+                {
+                    case "joined":
+                        if (!vloggerFollowers.ContainsKey(vlogerName))
+                        {
+                            vloggerFollowers[vlogerName] = new HashSet<string>();
+                        }
+
+                        if (!vloggerFolloed.ContainsKey(vlogerName))
+                        {
+                            vloggerFolloed[vlogerName] = new HashSet<string>();
+                        }
+
+                        break;
+                    case "followed":
+                        vloggerFollowers[vlogerName].Add(followed);
+                        vloggerFolloed[followed].Add(vlogerName); ;
+                        break;
+                }
+
 
 
 
