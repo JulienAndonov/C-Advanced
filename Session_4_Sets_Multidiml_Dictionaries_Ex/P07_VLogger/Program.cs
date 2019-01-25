16c797be7f68dbd664b271e558b0247d42cc6aa2
JulienAndonov <julien.v.andonov@googlemail.com>
2019-01-25 13:42:07 +0100

    25012019v6

--- Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
+++ Session_4_Sets_Multidiml_Dictionaries_Ex/P07_VLogger/Program.cs
@@ -8,11 +8,16 @@ namespace P07_VLogger
 
     class VloggerData
     {
-        public int followed { get; set; }
+        public int followers { get; set; }
         public int following { get; set; }
         public SortedSet<string> followersNames { get; set; }
 
-
+        public VloggerData(int followers, int following, SortedSet<string> followersNames)
+        {
+            this.followers = followers;
+            this.following = following;
+            this.followersNames = followersNames;
+        }
 
 
     }
@@ -21,8 +26,7 @@ namespace P07_VLogger
         static void Main(string[] args)
         {
             Dictionary<string, VloggerData> vloggers = new Dictionary<string, VloggerData>();
-
-
+            int index = 1;
 
             while (true)
             {
@@ -31,6 +35,21 @@ namespace P07_VLogger
 
                 if (entryCommand == "Statistics")
                 {
+                    Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+
+
+                    foreach (var vlogger in vloggers.OrderByDescending(x => x.Value.followers).ThenBy(x => x.Value.following))
+                    {
+                        Console.WriteLine($"{index}. {vlogger.Key} : {vlogger.Value.followers} followers, {vlogger.Value.following} following");
+                        if (index == 1)
+                        {
+                            foreach (var follower in vlogger.Value.followersNames)
+                            {
+                                Console.WriteLine($"*  {follower}");
+                            }
+                        }
+                        index++;
+                    }
                     break;
                 }
 
@@ -40,14 +59,28 @@ namespace P07_VLogger
                 var action = entries[1];
                 var scndName = entries[2];
 
-                
 
-
+                switch (action)
+                {
+                    case "joined":
+                        if (!vloggers.ContainsKey(vloggerName))
+                        {
+                            vloggers[vloggerName] = new VloggerData(0, 0, new SortedSet<string>());
+                        }
+                        break;
+                    case "followed":
+                        if (vloggers.ContainsKey(vloggerName)
+                            && vloggers.ContainsKey(scndName)
+                            && vloggerName != scndName
+                            && !vloggers[scndName].followersNames.Contains(vloggerName))
+                        {
+                            vloggers[vloggerName].following++;
+                            vloggers[scndName].followers++;
+                            vloggers[scndName].followersNames.Add(vloggerName);
+                        }
+                        break;
+                }
             }
-
-
-
-
         }
     }
 }
